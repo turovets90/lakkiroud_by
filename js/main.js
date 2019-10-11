6ea7bc0f41ab8e3dc8eea7305a948277a7c71fbc
turovets90 <a.turovets90@gmail.com>
2019-10-11 13:04:52 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -65,6 +65,21 @@ $(document).ready(function(){
     });
 
 
+    $('.amount .down').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.amount .up').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
 
 
 
